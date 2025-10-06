151352d44fd3b6d411536b3237b50c077a6f568b
Bilqees Shahid <161162316+BilqeesShahid@users.noreply.github.com>
2025-10-07 03:10:14 +0500

    Update main.py

--- main.py
+++ main.py
@@ -153,4 +153,7 @@ async def process_query(request: Request):
 # Run locally
 # -------------------
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=PORT)
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
+
